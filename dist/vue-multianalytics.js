51dc55dee673d6e864aecd92b8c54df8ab76da1b
Benoît Zugmeyer <bzugmeyer@gmail.com>
2018-06-06 13:54:07 +0200

    clean: remove async methods
    When using async functions, babel requires the "regeneratorRuntime"
    included (which is available either via `babel-polyfill` or
    `babel-plugin-transform-runtime`). A library should not expect the
    consumer to provide such a function (because babel may not be used at
    all in the final project). On top of that, the async/await functions
    can be trivialy changed to simple functions by removing some code.

--- dist/vue-multianalytics.js
+++ dist/vue-multianalytics.js
@@ -205,8 +205,6 @@ module.exports =
 
 	var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
-	function _asyncToGenerator(fn) { return function () { var gen = fn.apply(this, arguments); return new Promise(function (resolve, reject) { function step(key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { return Promise.resolve(value).then(function (value) { step("next", value); }, function (err) { step("throw", err); }); } } return step("next"); }); }; }
-
 	function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
 	/**
@@ -437,41 +435,17 @@ module.exports =
 
 	  }, {
 	    key: "setUserProperties",
-	    value: function () {
-	      var _ref = _asyncToGenerator( /*#__PURE__*/regeneratorRuntime.mark(function _callee() {
-	        var properties = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : {};
-	        var excludedModules = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : [];
-	        var modulesToExecute, response;
-	        return regeneratorRuntime.wrap(function _callee$(_context) {
-	          while (1) {
-	            switch (_context.prev = _context.next) {
-	              case 0:
-	                modulesToExecute = this.modulesEnabled.filter(function (moduleToCheck) {
-	                  return excludedModules.indexOf(moduleToCheck.name) === -1;
-	                });
-	                _context.next = 3;
-	                return Promise.all(modulesToExecute.map(function (module) {
-	                  return module.setUserProperties(properties);
-	                }));
+	    value: function setUserProperties() {
+	      var properties = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : {};
+	      var excludedModules = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : [];
 
-	              case 3:
-	                response = _context.sent;
-	                return _context.abrupt("return", response);
-
-	              case 5:
-	              case "end":
-	                return _context.stop();
-	            }
-	          }
-	        }, _callee, this);
+	      var modulesToExecute = this.modulesEnabled.filter(function (moduleToCheck) {
+	        return excludedModules.indexOf(moduleToCheck.name) === -1;
+	      });
+	      return Promise.all(modulesToExecute.map(function (module) {
+	        return module.setUserProperties(properties);
 	      }));
-
-	      function setUserProperties() {
-	        return _ref.apply(this, arguments);
-	      }
-
-	      return setUserProperties;
-	    }()
+	    }
 
 	    /**
 	     * Set some user properties once.
@@ -481,41 +455,17 @@ module.exports =
 
 	  }, {
 	    key: "setUserPropertiesOnce",
-	    value: function () {
-	      var _ref2 = _asyncToGenerator( /*#__PURE__*/regeneratorRuntime.mark(function _callee2() {
-	        var properties = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : {};
-	        var excludedModules = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : [];
-	        var modulesToExecute, response;
-	        return regeneratorRuntime.wrap(function _callee2$(_context2) {
-	          while (1) {
-	            switch (_context2.prev = _context2.next) {
-	              case 0:
-	                modulesToExecute = this.modulesEnabled.filter(function (moduleToCheck) {
-	                  return excludedModules.indexOf(moduleToCheck.name) === -1;
-	                });
-	                _context2.next = 3;
-	                return Promise.all(modulesToExecute.map(function (module) {
-	                  return module.setUserPropertiesOnce(properties);
-	                }));
+	    value: function setUserPropertiesOnce() {
+	      var properties = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : {};
+	      var excludedModules = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : [];
 
-	              case 3:
-	                response = _context2.sent;
-	                return _context2.abrupt("return", response);
-
-	              case 5:
-	              case "end":
-	                return _context2.stop();
-	            }
-	          }
-	        }, _callee2, this);
+	      var modulesToExecute = this.modulesEnabled.filter(function (moduleToCheck) {
+	        return excludedModules.indexOf(moduleToCheck.name) === -1;
+	      });
+	      return Promise.all(modulesToExecute.map(function (module) {
+	        return module.setUserPropertiesOnce(properties);
 	      }));
-
-	      function setUserPropertiesOnce() {
-	        return _ref2.apply(this, arguments);
-	      }
-
-	      return setUserPropertiesOnce;
-	    }()
+	    }
 
 	    /**
 	     * Set some user properties once.
@@ -564,41 +514,17 @@ module.exports =
 
 	  }, {
 	    key: "identify",
-	    value: function () {
-	      var _ref3 = _asyncToGenerator( /*#__PURE__*/regeneratorRuntime.mark(function _callee3() {
-	        var params = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : {};
-	        var excludedModules = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : [];
-	        var modulesToExecute, response;
-	        return regeneratorRuntime.wrap(function _callee3$(_context3) {
-	          while (1) {
-	            switch (_context3.prev = _context3.next) {
-	              case 0:
-	                modulesToExecute = this.modulesEnabled.filter(function (moduleToCheck) {
-	                  return excludedModules.indexOf(moduleToCheck.name) === -1;
-	                });
-	                _context3.next = 3;
-	                return Promise.all(modulesToExecute.map(function (module) {
-	                  return module.identify(params);
-	                }));
+	    value: function identify() {
+	      var params = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : {};
+	      var excludedModules = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : [];
 
-	              case 3:
-	                response = _context3.sent;
-	                return _context3.abrupt("return", response);
-
-	              case 5:
-	              case "end":
-	                return _context3.stop();
-	            }
-	          }
-	        }, _callee3, this);
+	      var modulesToExecute = this.modulesEnabled.filter(function (moduleToCheck) {
+	        return excludedModules.indexOf(moduleToCheck.name) === -1;
+	      });
+	      return Promise.all(modulesToExecute.map(function (module) {
+	        return module.identify(params);
 	      }));
-
-	      function identify() {
-	        return _ref3.apply(this, arguments);
-	      }
-
-	      return identify;
-	    }()
+	    }
 
 	    /**
 	     * Set an alias for the current instance
@@ -625,40 +551,16 @@ module.exports =
 
 	  }, {
 	    key: "reset",
-	    value: function () {
-	      var _ref4 = _asyncToGenerator( /*#__PURE__*/regeneratorRuntime.mark(function _callee4() {
-	        var excludedModules = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : [];
-	        var modulesToExecute, response;
-	        return regeneratorRuntime.wrap(function _callee4$(_context4) {
-	          while (1) {
-	            switch (_context4.prev = _context4.next) {
-	              case 0:
-	                modulesToExecute = this.modulesEnabled.filter(function (moduleToCheck) {
-	                  return excludedModules.indexOf(moduleToCheck.name) === -1;
-	                });
-	                _context4.next = 3;
-	                return Promise.all(modulesToExecute.map(function (module) {
-	                  return module.reset();
-	                }));
+	    value: function reset() {
+	      var excludedModules = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : [];
 
-	              case 3:
-	                response = _context4.sent;
-	                return _context4.abrupt("return", response);
-
-	              case 5:
-	              case "end":
-	                return _context4.stop();
-	            }
-	          }
-	        }, _callee4, this);
+	      var modulesToExecute = this.modulesEnabled.filter(function (moduleToCheck) {
+	        return excludedModules.indexOf(moduleToCheck.name) === -1;
+	      });
+	      return Promise.all(modulesToExecute.map(function (module) {
+	        return module.reset();
 	      }));
-
-	      function reset() {
-	        return _ref4.apply(this, arguments);
-	      }
-
-	      return reset;
-	    }()
+	    }
 	  }]);
 
 	  return AnalyticsPlugin;
--- src/AnalyticsPlugin.js
+++ src/AnalyticsPlugin.js
@@ -163,14 +163,13 @@ export default class AnalyticsPlugin {
                   *
                   * @param {any} properties - The user properties
                   */
-                 async setUserProperties (properties = {}, excludedModules = []) {
+                 setUserProperties (properties = {}, excludedModules = []) {
                    const modulesToExecute = this.modulesEnabled.filter(moduleToCheck => excludedModules.indexOf(moduleToCheck.name) === -1);
-                   const response = await Promise.all(modulesToExecute.map(
-                       module => {
-                         return module.setUserProperties(properties)
-                       }
-                     ));
-                   return response;
+                   return Promise.all(modulesToExecute.map(
+                     module => {
+                       return module.setUserProperties(properties)
+                     }
+                   ));
                  }
 
                  /**
@@ -178,16 +177,15 @@ export default class AnalyticsPlugin {
                   *
                   * @param {any} properties - The user properties once
                   */
-                 async setUserPropertiesOnce(properties = {}, excludedModules = []) {
+                 setUserPropertiesOnce(properties = {}, excludedModules = []) {
                    const modulesToExecute = this.modulesEnabled.filter(moduleToCheck => excludedModules.indexOf(moduleToCheck.name) === -1);
-                   const response = await Promise.all(modulesToExecute.map(
-                       module => {
-                         return module.setUserPropertiesOnce(
-                           properties
-                         );
-                       }
-                     ));
-                   return response;
+                   return Promise.all(modulesToExecute.map(
+                     module => {
+                       return module.setUserPropertiesOnce(
+                         properties
+                       );
+                     }
+                   ));
                  }
 
                  /**
@@ -222,14 +220,13 @@ export default class AnalyticsPlugin {
                   * @param {string} userId - The unique ID of the user
                   * @param {object} options - Options to add
                   */
-                 async identify(params = {}, excludedModules = []) {
+                 identify(params = {}, excludedModules = []) {
                    const modulesToExecute = this.modulesEnabled.filter(moduleToCheck => excludedModules.indexOf(moduleToCheck.name) === -1);
-                   const response = await Promise.all(modulesToExecute.map(
-                       module => {
-                         return module.identify(params);
-                       }
-                     ));
-                   return response;
+                   return Promise.all(modulesToExecute.map(
+                     module => {
+                       return module.identify(params);
+                     }
+                   ));
                  }
 
                  /**
@@ -249,13 +246,12 @@ export default class AnalyticsPlugin {
                   * Resets the id & clears cache
                   *
                   */
-                 async reset(excludedModules = []) {
+                 reset(excludedModules = []) {
                    const modulesToExecute = this.modulesEnabled.filter(moduleToCheck => excludedModules.indexOf(moduleToCheck.name) === -1);
-                   const response = await Promise.all(modulesToExecute.map(
-                       module => {
-                         return module.reset();
-                       }
-                     ));
-                   return response;
+                   return Promise.all(modulesToExecute.map(
+                     module => {
+                       return module.reset();
+                     }
+                   ));
                  }
                }
